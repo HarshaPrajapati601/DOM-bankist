8809f0c7cc39e8f3dc1d298abb187f692a816b47
Harsha Prajapati <harsha.prajapati@scripbox.com>
2022-06-12 15:02:22 +0530

    Event bubbling and capture

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -31,6 +34,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Implement smooth scrollinG
+btnScrollTo.addEventListener('click', function(e) {
+  //get the coordinates for section 1
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+ //current scrolling posiition
+  // console.log(window.pageXOffset, pageYOffset );
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //    s1coords.top + window.pageYOffset
+  //    );
+
+  //old way manually
+  // window.scrollTo({
+  //   left:  s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+    //new way  only for modern browsers
+  section1.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
+
+//page navigation
+
+document.querySelectorAll('.nav__link').forEach(function(ele) {
+  ele.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("linkss")
+  })
+})
+ //returns a nodelist
+ 
+
+
+
+
+
 
 // CRUD OPERATIONS --------------------------
 
@@ -81,32 +126,54 @@ logo.getAttribute('src')
 //Data attributtes --start with data
 
 
-//Implement smooth scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function(e) {
-  //get the coordinates for section 1
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-  console.log(e.target.getBoundingClientRect());
- //current scrolling posiition
-  // console.log(window.pageXOffset, pageYOffset );
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //    s1coords.top + window.pageYOffset
-  //    );
 
-  //old way manually
-  // window.scrollTo({
-  //   left:  s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth'
-  // })
+//Events - s//Mouse enter
 
-    //new way  only for modern browsers
-  section1.scrollIntoView({
-    behavior: 'smooth'
-  })
+const h1  = document.querySelector('h1');
+const alertH1 =  function(e) {
+  alert("from heading")
+};
+//this is better to listen for the event once
+h1.addEventListener('mouseenter', alertH1 );
+
+// //for mouseenter --old school
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1
+), 2000);
+
+// h1.onmouseenter = function(e) {
+//   alert("from heading")
+// }
+
+//Event bubbling
+
+const randomColorGenerator = (min, max) => Math.floor(Math.random() * ( max - min + 1) + min);
+
+const randomColor = (e) =>  `rgb(${randomColorGenerator(0, 122)}, ${randomColorGenerator(0, 255)}, ${randomColorGenerator(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+// console.log("1", e.target, e.currentTarget)
+
+//stop propagation
+e.stopPropagation();
 })
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  // console.log("2", e.target, e.currentTarget)
+  // this.style.backgroundColor = randomColor();
+})
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+  // this.style.backgroundColor = randomColor();
+  // console.log("3", e.target, e.currentTarget)
+})
+
+ //to listen to capture events
+// document.querySelector('.nav').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("3", e.target, e.currentTarget)
+// }, true)
+
+
 
